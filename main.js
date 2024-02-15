532bafc870d74ab481c27ce00141bcfc0dc8d086
vkstheone <89942870+vkstheone@users.noreply.github.com>
2024-02-14 21:05:41 -0300

    configurando pagina de detalhes

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ let offset = 0;
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
+        <a href="${pokemon.name}">
+
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -19,6 +21,7 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+        </a>
         </li>
     `
 }
